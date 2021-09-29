e32a136f642443156beccd6c03dbe26d47bbd7b9
XanAstia <49984348+XanAstia@users.noreply.github.com>
2021-09-29 18:20:04 +0200

    Creation du package
    Ajout du script contenant le décorateur et ajout des sons de Kaamelott.

---
+++ nectar.py
@@ -0,0 +1,20 @@
+#With great power comes great responsibility.
+
+from glob import glob
+import os
+import random
+
+def Nectar(function):
+
+    def new_fuction(*args, **kwargs):
+        try:
+            function(*args, **kwargs)
+            files = glob('./Sounds/Kaamelott/Succes/*.wav')
+            file = random.choice(files)
+            os.system('aplay ' + file)
+        except:
+            files = glob('./Sounds/Kaamelott/Echec/*.wav')
+            file = random.choice(files)
+            os.system('aplay ' + file)
+
+    return new_fuction
