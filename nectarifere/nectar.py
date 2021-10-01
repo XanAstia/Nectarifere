cc6640b1fe42a07fd0ba2bffd93518ea22839786
sdupourque <sdupourque@irap.omp.eu>
2021-10-01 13:59:04 +0200

    Various optimization: -File list loading once when creating the function -Add finally clause

--- nectarifere/nectar.py
+++ nectarifere/nectar.py
@@ -8,15 +8,20 @@ directory = os.path.dirname(os.path.abspath(__file__))
 
 def Nectar(function):
 
+    success_files = glob(os.path.join(directory, 'Sounds/Kaamelott/Succes/*.wav'))
+    fail_files = glob(os.path.join(directory, 'Sounds/Kaamelott/Echec/*.wav'))
+
     def new_function(*args, **kwargs):
+
         try:
             function(*args, **kwargs)
-            files = glob(os.path.join(directory, 'Sounds/Kaamelott/Succes/*.wav'))
-            file = random.choice(files)
-            playsound(file)
+            file = random.choice(success_files)
+
         except:
-            files = glob(os.path.join(directory ,'Sounds/Kaamelott/Echec/*.wav'))
-            file = random.choice(files)
+
+            file = random.choice(fail_files)
+
+        finally:
             playsound(file)
 
     return new_function
