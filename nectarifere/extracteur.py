a62aa819c2f853cc211fa13ba4abf2a73ba993b8
sdupourque <sdupourque@irap.omp.eu>
2021-11-12 16:00:41 +0100

    A bit of documentation

--- nectarifere/extracteur.py
+++ nectarifere/extracteur.py
@@ -2,27 +2,37 @@ from abc import ABC, abstractmethod
 
 
 class ExtracteurDeJus(ABC):
+    """
+    Abstract class for creating success/failure patterns with arbitrary actions
+    """
 
     def __init__(self, function):
-        self.function = function
+        self._function = function
 
     @abstractmethod
     def success(self):
+        '''
+        Abstract method called when the decorated function succeeds
+        '''
         pass
 
     @abstractmethod
     def failure(self):
+        '''
+        Abstract method called when the decorated function fails
+        '''
         pass
 
-    def __call__(self,*args, **kwargs):
-
-
+    def __call__(self, *args, **kwargs):
+        '''
+        Decorating the function call
+        '''
         caught_exception = None
         res = None
 
         try:
 
-            res = self.function(*args, **kwargs)
+            res = self._function(*args, **kwargs)
             self.success()
 
         except Exception as e:
